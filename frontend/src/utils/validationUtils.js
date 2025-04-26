fa815a5ea8d9d911bc0291bb415126f3b69b2a1a
BoduBhai <bodruddozaaraf@gmail.com>
2025-04-26 20:32:17 +0600

    Contact Form

--- frontend/src/utils/validationUtils.js
+++ frontend/src/utils/validationUtils.js
@@ -16,3 +16,18 @@ export const validateSignupForm = (values) => {
 
   return errors;
 };
+
+export const validateContactForm = (values) => {
+  const errors = {};
+
+  if (!values.name) errors.name = "Name is required";
+
+  if (!values.email) errors.email = "Email is required";
+  if (values.email && !/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (!values.message) errors.message = "Message is required";
+
+  return errors;
+};
